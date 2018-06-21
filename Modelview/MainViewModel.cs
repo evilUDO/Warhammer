29455c051b279f92202fee3dc70ed3964f7c7eff
pauliniklas <pauliniklas@U12WKS05>
2018-06-21 13:48:04 +0200

    Revert "Revert "ein paar änderungen""
    This reverts commit 9d2b11477c29985ada129b5773e15a0afb0f9e34.

--- Modelview/MainViewModel.cs
+++ Modelview/MainViewModel.cs
@@ -4,16 +4,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Viewmodel
 {
     public class MainViewModel
     {
         Kampf warhammerKampf = null;
+        private ICommand addGegCommand;
+
+        public ICommand AddGegCommand
+        {
+            get
+            {
+                return addGegCommand;
+            }
+
+            set
+            {
+                addGegCommand = value;
+            }
+        }
 
         public MainViewModel()
         {
            warhammerKampf = new Kampf();
+            this.AddGegCommand = new UserCommand(new Action<object>(AddGegner));
+        }
+
+        private void AddGegner(Object obj)
+        {
+            Geschoepf gesch = (Geschoepf)obj;
+            warhammerKampf.FuegeGegnerHinzu(gesch);
         }
 
     }
